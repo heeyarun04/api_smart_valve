e39ebaee68114a920f64adbe6a85d718e5aaae62
Hiyarunss <144218408+heeyarun04@users.noreply.github.com>
2023-12-14 06:16:49 +0000

    Update controller.js and package-lock.json

--- controller/controller.js
+++ controller/controller.js
@@ -1,11 +1,9 @@
 async function reqToken(req, res) {
-
+  const user = {};
 }
-
 
 async function inputData(req, res) {
   const { token, volume, debit } = req.body;
-
 }
 
 async function dashboard(req, res) {}
